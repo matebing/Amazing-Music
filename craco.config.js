de822a2d70cb128457e7f1ce2b54e84ad219b661
张兵 <1691635403@qq.com>
2025-12-08 22:17:26 +0800

    项目搭建，完成基础配置，如别名等

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+//定义resolve函数，把当前目录 __dirname 和上传进来的目录 dir 拼接起来
+//对于当前文件来说，当前目录就是项目根目录
+const resolve = (dir) => path.resolve(__dirname, dir);
+
+module.exports = {
+    webpack: {
+        //配置别名
+        alias:{
+            '@': resolve('src'),
+            components: resolve('src/components')
+        }
+    }
+}
