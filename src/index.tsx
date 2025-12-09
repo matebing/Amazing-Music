0e1ad38d53c7410665dea327b1c14eb33acea368
张兵 <1691635403@qq.com>
2025-12-09 23:47:27 +0800

    划分目录结构，重置样式等

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//对css样式进行重置，有些原生样式不是我们想要的效果
+import 'normalize.css';
+import './assets/css/index.less';
+
 import App from '@/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 //渲染APP
 root.render(<App />);
