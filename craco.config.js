dd2935ebb1379a74117a346b2bfd893c99e383e9
张兵 <1691635403@qq.com>
2025-12-13 11:14:23 +0800

    格式化项目代码

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const CracoLessPlugin = require('craco-less');
+const path = require('path')
+const CracoLessPlugin = require('craco-less')
 
 //定义resolve函数，把当前目录 __dirname 和上传进来的目录 dir 拼接起来
 //对于当前文件来说，当前目录就是项目根目录
-const resolve = (dir) => path.resolve(__dirname, dir);
+const resolve = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
   plugins: [
@@ -18,4 +18,4 @@ module.exports = {
       components: resolve('src/components')
     }
   }
-};
+}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
@@ -13,4 +13,4 @@ export default defineConfig([
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended
-]);
+])
