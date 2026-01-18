967aea16d7d2e627c3db0d968cbf4619b6bf4e27
张兵 <1691635403@qq.com>
2026-01-18 15:26:13 +0800

    热门主播功能开发

--- src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
@@ -1,13 +1,33 @@
 import { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { HotAnchorWrapp } from './style'
+import { hotRadios } from '@/assets/data/local_data'
 
 interface IProps {
   children?: ReactNode
 }
 
 const HotAnchor: FC<IProps> = (props) => {
-  return <HotAnchorWrapp>HotAnchor</HotAnchorWrapp>
+  return (
+    <HotAnchorWrapp>
+      <div className="hot-anchor-herder">
+        <h3 className="title">热门主播</h3>
+      </div>
+      <div className="anchors">
+        {hotRadios.map((item, index) => {
+          return (
+            <div className="item" key={index}>
+              <img className="image" src={item.picUrl} alt=""></img>
+              <div className="info">
+                <div className="name">{item.name}</div>
+                <div className="position">{item.position}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </HotAnchorWrapp>
+  )
 }
 
 //直接导出性能不高，即便props没有改变，组件也会重新渲染，使用memo可避免此问题
--- src/views/discover/c-views/recommend/c-cpns/hot-anchor/style.ts
+++ src/views/discover/c-views/recommend/c-cpns/hot-anchor/style.ts
@@ -1,2 +1,58 @@
 import styled from 'styled-components'
-export const HotAnchorWrapp = styled.div``
+export const HotAnchorWrapp = styled.div`
+  padding: 20px;
+  .hot-anchor-herder {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding-bottom: 5px;
+    border-bottom: 1px solid #ccc;
+
+    .title {
+      font-size: 12px;
+      color: #666666;
+      font-weight: 700;
+    }
+  }
+  .anchors {
+    margin: 20px;
+
+    .item {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
+
+      cursor: pointer;
+
+      &:hover {
+        .info .name {
+          text-decoration: underline;
+        }
+      }
+
+      img {
+        width: 40px;
+        height: 40px;
+        object-fit: cover;
+      }
+
+      .info {
+        flex: 1 0 auto;
+        width: 0;
+        padding-left: 8px;
+
+        .name {
+          padding: 0 0 7px 0;
+          color: #000;
+          ${(props) => (props.theme as any).mixin.textEllipsis};
+        }
+
+        .position {
+          font-size: 12px;
+          color: ${(props) => (props.theme as any).color.secondary};
+          ${(props) => (props.theme as any).mixin.textEllipsis};
+        }
+      }
+    }
+  }
+`
